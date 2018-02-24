6f80c21a4c69c324cca317663531df4c2ea95f20
Enopoletus <33143044+Enopoletus@users.noreply.github.com>
2018-02-24 18:51:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 window.addEventListener("load",
+document.getElementById("form1").onsubmit=function() {
+           q1 = parseInt(document.querySelector('input[name = "question"]:checked').value);
+	   q2 = parseInt(document.querySelector('input[name = "question2"]:checked').value);
+	   q3 = parseInt(document.querySelector('input[name = "question3"]:checked').value);
+	   q4 = parseInt(document.querySelector('input[name = "question4"]:checked').value);
+	   result = q1 + q2 + q3 + q4;
+	document.getElementById("grade").innerHTML = result;
+  return false;}
 );
