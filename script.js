cb489f95062d169e5c24bb1f5ff46da24f6b8b74
Enopoletus <33143044+Enopoletus@users.noreply.github.com>
2018-02-24 18:40:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,8 @@ document.getElementById("form1").onsubmit=function() {
 	   q2 = parseInt(document.querySelector('input[name = "question2"]:checked').value);
 	   q3 = parseInt(document.querySelector('input[name = "question3"]:checked').value);
 	   q4 = parseInt(document.querySelector('input[name = "question4"]:checked').value);
-	   
-	   
 	   result = q1 + q2 + q3 + q4;
-	   
 	document.getElementById("grade").innerHTML = result;
-	   
-
-
-return false; // required to not refresh the page; just leave this here
-} //this ends the submit function
+return false;
+}
 );
