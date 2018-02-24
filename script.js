bba24d04199b8dcdd70171336ffe9b8d40eca656
Enopoletus <33143044+Enopoletus@users.noreply.github.com>
2018-02-24 18:32:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener("load",
 document.getElementById("form1").onsubmit=function() {
-           q1 = parseInt(document.querySelector('input[name = "question1"]:checked').value);
+           q1 = parseInt(document.querySelector('input[name = "question"]:checked').value);
 	   q2 = parseInt(document.querySelector('input[name = "question2"]:checked').value);
 	   q3 = parseInt(document.querySelector('input[name = "question3"]:checked').value);
 	   q4 = parseInt(document.querySelector('input[name = "question4"]:checked').value);
