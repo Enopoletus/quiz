99650c1539c7fab1a4caa2b272e0d3aecefb7c50
Enopoletus <33143044+Enopoletus@users.noreply.github.com>
2018-02-24 21:05:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 window.addEventListener("load", function nine(){
 xz = document.querySelector('input[name="question"]:checked').value;
-document.getElementById("demo").innerHTML = xz;}
+document.getElementById("grade").innerHTML = xz;}
 );
