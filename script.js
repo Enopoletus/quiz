e99593f419219413c2aa59bbc526fdca0feba03d
Enopoletus <33143044+Enopoletus@users.noreply.github.com>
2018-02-24 22:43:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,7 @@ x3 = parseFloat(document.querySelector('input[name="question3"]:checked').value)
 x4 = parseFloat(document.querySelector('input[name="question4"]:checked').value);
 x5 = parseFloat(document.querySelector('input[name="question5"]:checked').value);
 x6 = parseFloat(document.querySelector('input[name="question6"]:checked').value);
-document.getElementById("grade").innerHTML = 100*String((x1+x2+x3+x4+x5+x6)/6);}
+var arrs=[x1, x2, x3, x4, x5, x6]
+var arrsum = arrs.reduce((a, b) => a + b, 0)
+document.getElementById("grade").innerHTML = 100*String((arrsum)/(arrs.length);}
 );
