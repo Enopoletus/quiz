47de01fb8f19d8fb29a8c68ce68634a0d357d60d
Enopoletus <33143044+Enopoletus@users.noreply.github.com>
2018-02-24 20:56:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 window.addEventListener("load",
-xz = document.querySelector('input[name="rate"]:checked').value;
+xz = document.querySelector('input[name="question"]:checked').value;
 document.getElementById("demo").innerHTML = xz;
 );
