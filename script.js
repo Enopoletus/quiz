21a8d208bbff6a97e0a3b8a783f52ccd66f37bdf
Enopoletus Harding <33143044+Enopoletus@users.noreply.github.com>
2018-02-25 22:03:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const exl = document.getElementsByClassName('answ');
 for(let i = 0; i < exl.length; i++) {
 if (exl[i].checked){ 
 let zed = exl[i];
-array.push(parseFloat(zed.value));
+array.push(parseFloat(zed.value));}}
 var arrsum = array.reduce((a, b) => a + b, 0);
-document.getElementById("grade").innerHTML = 100*String((arrsum)/(array.length));}}}
+document.getElementById("grade").innerHTML = 100*String((arrsum)/(array.length));}
 );
