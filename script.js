7997a20d821c47d093ae521cfc0dfc7c5cb8de5c
Enopoletus Harding <33143044+Enopoletus@users.noreply.github.com>
2018-02-28 12:59:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 window.addEventListener("click", function n8ne(){
 let arr8y = [];
+let arr7y = [];
 const exl = document.getElementsByClassName('answ');
 for(let i = 0; i < exl.length; i++) {
 if (exl[i].checked){
 arr8y.push(parseFloat(exl[i].value));}}
+for(let i = 0; i < exl.length; i++) {
+arr7y.push(parseFloat(exl[i].value));}
 const arrsum = arr8y.reduce((a, b) => a + b, 0);
-document.getElementById("grade").innerHTML = 100*String((arrsum)/(arr8y.length));}
+document.getElementById("grade").innerHTML = 100*String((arrsum)/(arr7y.length));}
 );
