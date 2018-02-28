a071ec079147d24234910ba9ce3164cd31438a7d
Enopoletus Harding <33143044+Enopoletus@users.noreply.github.com>
2018-02-28 14:01:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.getElementById("grade").innerHTML = 100*String((arrsum)/(arr8y.length))
 var divs = document.getElementsByTagName('input');
 var numTabs = 0;
 for (var i = 0; i < divs.length; i++) {
-	if (divs[i].class.indexOf('question') != -1)
+	if (divs[i].className.indexOf('question') != -1)
 		numTabs++;}
 document.getElementById("c0unt").innerHTML = (String(arr8y.length));
 document.getElementById("c00unt").innerHTML = (String(numTabs));
