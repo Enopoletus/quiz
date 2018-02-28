3e5a746f84896629257265f122b098f10447ef4a
Enopoletus Harding <33143044+Enopoletus@users.noreply.github.com>
2018-02-28 14:55:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const arrsum = arr8y.reduce((a, b) => a + b, 0);
 document.getElementById("grade").innerHTML = 100*String((arrsum)/(arr8y.length));
 var divs = document.getElementById('form1').children;
 var numTabs = 0;
-for (var i = 0; i < divs.length; i++) {
-	if (divs[i].tagName('p') != -1)
+for(let x = 0; x < divs.length; x++) {
+	if (divs[x].tagName.indexOf('p') != -1)
 		numTabs++;}
 document.getElementById("c0unt").innerHTML = (String(arr8y.length));
 document.getElementById("c00unt").innerHTML = (String(numTabs));
