c3feb1363243206d009c3edfb2f7f5f4f5a960da
Enopoletus Harding <33143044+Enopoletus@users.noreply.github.com>
2018-02-28 14:51:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,11 @@ if (exl[i].checked){
 arr8y.push(parseFloat(exl[i].value));}};
 const arrsum = arr8y.reduce((a, b) => a + b, 0);
 document.getElementById("grade").innerHTML = 100*String((arrsum)/(arr8y.length));
-	for (document.getElementById('form1').children){
-var divs = document.getElementsByTagName('input');
+var divs = document.getElementById('form1').children;
 var numTabs = 0;
 for (var i = 0; i < divs.length; i++) {
-	if (divs[i].name.indexOf('question') != -1)
-		numTabs++;}}
+	if (divs[i].tagName('p') != -1)
+		numTabs++;}
 document.getElementById("c0unt").innerHTML = (String(arr8y.length));
 document.getElementById("c00unt").innerHTML = (String(numTabs));
 });
