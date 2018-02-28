205eabae26ca248303b8f70bf891e9a3a7bb36fe
Enopoletus Harding <33143044+Enopoletus@users.noreply.github.com>
2018-02-28 13:01:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let arr7y = [];
 const exl = document.getElementsByClassName('answ');
 for(let i = 0; i < exl.length; i++) {
 if (exl[i].checked){
-arr8y.push(parseFloat(exl[i].value));}}
+arr8y.push(parseFloat(exl[i].value));}};
 for(let i = 0; i < exl.length; i++) {
 arr7y.push(parseFloat(exl[i].value));}
 const arrsum = arr8y.reduce((a, b) => a + b, 0);
