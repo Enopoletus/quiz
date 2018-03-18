24eabb192a1763e775a667bdcf2de08cd50b4d75
Enopoletus Harding <33143044+Enopoletus@users.noreply.github.com>
2018-03-17 21:51:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -21,8 +21,18 @@ document.getElementById("c0unt2").innerHTML = (String(arr6y.length));
 document.getElementById("c00unt2").innerHTML = (String(numTabs2));
 const w = (screen.width)/3;
 const h = (screen.width)/3;
+var data = [10, 15, 20, 25, 30];
 let svg = d3.select("#ten10")
             .append('svg')
             .attr('width', w)
             .attr('height', h);
+var scale = d3.scaleLinear()
+              .domain([d3.min(data), d3.max(data)])
+              .range([height/2, 0]);
+var y_axis = d3.axisLeft()
+               .scale(scale);
+svg.append("g")
+       .attr("transform", "translate(50, 10)")
+       .call(y_axis);
+
 });
