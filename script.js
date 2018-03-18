8199cd867b78dbf9ecf2d173084a3a48308e9fdd
Enopoletus Harding <33143044+Enopoletus@users.noreply.github.com>
2018-03-17 22:52:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+window.addEventListener("click", function n8ne(){
+let arr8y = [];
+const exl = document.getElementsByClassName('answ');
+for(let i = 0; i < exl.length; i++) {
+if (exl[i].checked){
+arr8y.push(parseFloat(exl[i].value));}};
+const arrsum = arr8y.reduce((a, b) => a + b, 0);
+document.getElementById("grade").innerHTML = 100*String((arrsum)/(arr8y.length));
+var numTabs=document.getElementById("form1").getElementsByTagName("P").length;
+document.getElementById("c0unt").innerHTML = (String(arr8y.length));
+document.getElementById("c00unt").innerHTML = (String(numTabs));
+let arr6y = [];
+const exl2 = document.getElementsByClassName('answ2');
+for(let i = 0; i < exl2.length; i++) {
+if (exl2[i].checked){
+arr6y.push(parseFloat(exl2[i].value));}};
+const arrsum2 = arr6y.reduce((a, b) => a + b, 0);
+document.getElementById("grade2").innerHTML = 100*String((arrsum2)/(arr6y.length));
+var numTabs2=document.getElementById("form2").getElementsByTagName("P").length;
+document.getElementById("c0unt2").innerHTML = (String(arr6y.length));
+document.getElementById("c00unt2").innerHTML = (String(numTabs2));
+});
 window.addEventListener("click", handler);
 function handler(e) {
 	e.target.removeEventListener(e.type, arguments.callee);
@@ -24,25 +46,3 @@ svg.append("g")
        .attr("transform", "translate(40, 40)")
        .call(y_axis);
 };
-window.addEventListener("click", function n8ne(){
-let arr8y = [];
-const exl = document.getElementsByClassName('answ');
-for(let i = 0; i < exl.length; i++) {
-if (exl[i].checked){
-arr8y.push(parseFloat(exl[i].value));}};
-const arrsum = arr8y.reduce((a, b) => a + b, 0);
-document.getElementById("grade").innerHTML = 100*String((arrsum)/(arr8y.length));
-var numTabs=document.getElementById("form1").getElementsByTagName("P").length;
-document.getElementById("c0unt").innerHTML = (String(arr8y.length));
-document.getElementById("c00unt").innerHTML = (String(numTabs));
-let arr6y = [];
-const exl2 = document.getElementsByClassName('answ2');
-for(let i = 0; i < exl2.length; i++) {
-if (exl2[i].checked){
-arr6y.push(parseFloat(exl2[i].value));}};
-const arrsum2 = arr6y.reduce((a, b) => a + b, 0);
-document.getElementById("grade2").innerHTML = 100*String((arrsum2)/(arr6y.length));
-var numTabs2=document.getElementById("form2").getElementsByTagName("P").length;
-document.getElementById("c0unt2").innerHTML = (String(arr6y.length));
-document.getElementById("c00unt2").innerHTML = (String(numTabs2));
-});
