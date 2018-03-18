5ee1fe2b6dd458e577c726774f07a5c6a20640ae
Enopoletus Harding <33143044+Enopoletus@users.noreply.github.com>
2018-03-17 22:29:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -38,10 +38,10 @@ const y_axis = d3.axisLeft()
             .scale(yscale);
 const xaxistranslate=h/2+10;
 svg.append("g")
-       .attr("transform", "translate(0, " + xaxistranslate +")")
+       .attr("transform", "translate(10, " + xaxistranslate +")")
        .call(x_axis);
 svg.append("g")
-       .attr("transform", "translate(0, 10)")
+       .attr("transform", "translate(10, 10)")
        .call(y_axis);
 
 });
