e1274fac40331a2029ef722d6cdb2e1cd932332d
Enopoletus Harding <33143044+Enopoletus@users.noreply.github.com>
2018-05-16 23:02:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -58,6 +58,19 @@ svg.append('circle')
     .attr('cx', 0)
     .attr('cy', 0)
     .style("fill", "lightgreen");
+svg.append("text")             
+      .attr("transform",
+            "translate(" + (width/2) + " ," + 
+                           (height + margin.top + 20) + ")")
+      .style("text-anchor", "middle")
+      .text("Date");
+svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("Value"); 
 window.addEventListener("click",
 function frame(){
 svg.select('circle')                
